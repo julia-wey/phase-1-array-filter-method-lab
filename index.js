19801e4be8c4f28a9d8232b50c3436b09376a66c
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-09-27 18:51:00 -0400

    Adds Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const lowercaseName = name.toLowerCase();
+  
+    const matchingDrivers = drivers.filter(function(driver) {
+        return driver.toLowerCase() === lowercaseName;
+    });
+    return matchingDrivers;
+  }
+
+  function fuzzyMatch(drivers, letter) {
+     const firstLetter = drivers.filter(function(driver) {
+        return driver.startsWith(letter)})
+      return firstLetter}
+
+  function matchName(drivers, value) {
+    return drivers.filter(function(driver) {
+      return driver.name === value})
+  }
+     
+
+    
